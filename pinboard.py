ab96998ec08ec36b203a7bbcf3dc17c411ea188f
Alex Galonsky <alex@galonsky.com>
2014-02-24 19:20:42 -0800

    Adding ignored and fixed tag

--- pinboard.py
+++ pinboard.py
@@ -5,14 +5,17 @@ from cookielib import CookieJar
 config = ConfigParser.ConfigParser()
 config.read(['auth.cfg'])
 API_KEY = config.get('auth', 'token')
+IGNORE_TAG = 'dont_fix'
+FIXED_TAG = 'fixed'
 
 socket.setdefaulttimeout(3)
 
-def add_bookmark(url, title, datetime):
+def add_bookmark(url, title, datetime, tags):
     params = {
         'url': url,
         'description': title.encode('utf8'),
         'dt': datetime,
+        'tags': tags,
         'format': 'json',
         'auth_token': API_KEY
     }
@@ -48,7 +51,14 @@ def has_html_entities(title):
     h = HTMLParser.HTMLParser()
     return title != h.unescape(title)
 
-def should_process(title):
+def should_ignore(tags):
+    return IGNORE_TAG in tags.split()
+
+def should_process(bookmark):
+    title = bookmark['description']
+    if should_ignore(bookmark['tags']):
+        print 'ignoring ' + bookmark['description']
+        return False;
     return title == 'Untitled' or 'http://' in title or has_html_entities(title)
 
 def is_html(headers):
@@ -57,11 +67,23 @@ def is_html(headers):
             return True;
     return False;
 
+def add_tag(tags, tag):
+    tags_list = tags.split()
+    tags_list.append(tag)
+    return ' '.join(tags_list)
+
+def already_processed(tags):
+    tags_list = tags.split()
+    return IGNORE_TAG in tags_list or FIXED_TAG in tags_list
+
 def fix_bookmarks():
     bookmarks = get_bookmarks()
     count = 0
     for bookmark in bookmarks:
-        if should_process(bookmark['description']):
+        if (already_processed(bookmark['tags'])):
+            # we've already processed up to this point
+            break
+        if should_process(bookmark):
             print "should process"
             count = count + 1
             try:
@@ -69,12 +91,14 @@ def fix_bookmarks():
                 title = get_title(bookmark['href'])
                 print "end title"
             except:
+                # Add ignore tag
+                add_bookmark(bookmark['href'], bookmark['description'], bookmark['time'], add_tag(bookmark['tags'], IGNORE_TAG))
                 continue
             print bookmark['href'] + ' ' + title
             print "sleeping"
             time.sleep(3)
             print "adding"
-            add_bookmark(bookmark['href'], title, bookmark['time'])
+            add_bookmark(bookmark['href'], title, bookmark['time'], add_tag(bookmark['tags'], FIXED_TAG))
             print "done adding"
         else:
             print "should not process"
